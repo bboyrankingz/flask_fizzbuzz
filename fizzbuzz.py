da8c079b92fe484c9becae73aa488ad087503ba8
bboy.stats <bboy.stats@gmail.com>
2017-05-23 22:49:21 +0200

    classic fizzbuzz from 1 to 100

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,6 +8,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route("/fizzbuzz/")
+def fizzbuzz():
+    fizzbuzz = [("fizz" if i % 3 == 0 else "") + ("buzz" if i % 5 == 0 else "") or i for i in xrange(1, 101)]
+    return json.dumps(fizzbuzz)
 
 if __name__ == "__main__":
     app.run()
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import unittest
+import json
+
 import fizzbuzz
 
 
@@ -10,6 +12,14 @@ class FizzBuzzTestCase(unittest.TestCase):
     def test_hello_world(self):
         rv = self.app.get('/')
         self.assertEqual(rv.data, "Hello World!")
+
+    def test_classic_fizzbuzz(self):
+        rv = self.app.get('/fizzbuzz/')
+        fizzbuzz = json.loads(rv.data)
+        self.assertEqual(len(fizzbuzz), 100)
+        self.assertEqual(fizzbuzz[2], "fizz")
+        self.assertEqual(fizzbuzz[4], "buzz")
+        self.assertEqual(fizzbuzz[14], "fizzbuzz")
 
 if __name__ == '__main__':
     unittest.main()
